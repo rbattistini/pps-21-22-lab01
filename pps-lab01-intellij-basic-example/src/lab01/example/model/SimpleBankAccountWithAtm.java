b7cb9ef959bcdb2c183cfd878a31b8c6cd42dcac
Riccardo Battistini <riccardo.battistini2@studio.unibo.it>
2022-03-01 20:49:25 +0100

    Update SimpleBankAccountWithAtm and related test

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -1,6 +1,49 @@
 package lab01.example.model;
 
 public class SimpleBankAccountWithAtm implements BankAccount {
-    public SimpleBankAccountWithAtm(AccountHolder accountHolder, int i) {
+
+    private double balance;
+    private final AccountHolder holder;
+
+    public SimpleBankAccountWithAtm(final AccountHolder holder, final double balance) {
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    @Override
+    public AccountHolder getHolder(){
+        return this.holder;
+    }
+
+    @Override
+    public double getBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public void deposit(final int userID, final double amount) {
+        if (checkUser(userID) && checkNonNegativeAmount(amount)) {
+            this.balance += amount;
+        }
+    }
+
+    @Override
+    public void withdraw(final int userID, final double amount) {
+        if (checkUser(userID) && checkNonNegativeAmount(amount) &&
+                isWithdrawAllowed(amount)) {
+            this.balance = this.balance - amount - 1.0;
+        }
+    }
+
+    private boolean checkNonNegativeAmount(final double amount) {
+        return amount >= 0;
+    }
+
+    private boolean isWithdrawAllowed(final double amount){
+        return this.balance >= amount;
+    }
+
+    private boolean checkUser(final int id) {
+        return this.holder.getId() == id;
     }
 }
--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class SimpleBankAccountWithAtmTest {
+import lab01.example.model.AccountHolder;
+import lab01.example.model.BankAccount;
+import lab01.example.model.SimpleBankAccountWithAtm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The test suite for testing the SimpleBankAccountWithAtm implementation
+ */
+public class SimpleBankAccountWithAtmTest {
+
+    private static final int INVALID_USER_ID = 2;
+    private static final double EMPTY_BALANCE = 0.0;
+    private static final double DUMMY_DEPOSIT = 100.0;
+    private static final double DUMMY_WITHDRAW = 70.0;
+    private static final double FEE = 1.0;
+    private AccountHolder accountHolder;
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    void beforeEach(){
+        int userId = 1;
+        accountHolder = new AccountHolder("Mario", "Rossi", userId);
+        bankAccount = new SimpleBankAccountWithAtm(accountHolder, EMPTY_BALANCE);
+    }
+
+    @Test
+    void testInitialBalance() {
+        assertEquals(EMPTY_BALANCE, bankAccount.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        bankAccount.deposit(accountHolder.getId(), DUMMY_DEPOSIT);
+        assertEquals(DUMMY_DEPOSIT, bankAccount.getBalance());
+    }
+
+    @Test
+    void testNegativeDeposit() {
+        bankAccount.deposit(accountHolder.getId(), -DUMMY_DEPOSIT);
+        assertEquals(EMPTY_BALANCE, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWrongIdDeposit() {
+        bankAccount.deposit(INVALID_USER_ID, DUMMY_DEPOSIT);
+        assertEquals(EMPTY_BALANCE, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        double depositedAmount = 100.0;
+        double withdrawnAmount = 80.0;
+        bankAccount.deposit(accountHolder.getId(), depositedAmount);
+        bankAccount.withdraw(accountHolder.getId(), withdrawnAmount);
+        assertEquals(depositedAmount - withdrawnAmount - FEE, bankAccount.getBalance());
+    }
+
+    @Test
+    void testWrongIdWithdraw() {
+        bankAccount.deposit(accountHolder.getId(), DUMMY_DEPOSIT);
+        bankAccount.withdraw(INVALID_USER_ID, DUMMY_WITHDRAW);
+        assertEquals(DUMMY_DEPOSIT, bankAccount.getBalance());
+    }
+
+    @Test
+    void testNegativeWithdraw() {
+        bankAccount.deposit(accountHolder.getId(), DUMMY_DEPOSIT);
+        bankAccount.withdraw(accountHolder.getId(), -DUMMY_WITHDRAW);
+        assertEquals(DUMMY_DEPOSIT, bankAccount.getBalance());
+    }
+
+    @Test
+    void testDisallowedWithdraw() {
+        final double depositedAmount = 100.0;
+        final double withdrawnAmount = 120.0;
+        bankAccount.deposit(accountHolder.getId(), depositedAmount);
+        bankAccount.withdraw(INVALID_USER_ID, withdrawnAmount);
+        assertEquals(depositedAmount, bankAccount.getBalance());
+    }
 }
