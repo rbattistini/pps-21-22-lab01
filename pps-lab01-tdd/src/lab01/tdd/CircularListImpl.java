383400f61a76f662bb429d5741ed1c1f5c32fec1
Riccardo Battistini <riccardo.battistini2@studio.unibo.it>
2022-03-02 15:18:42 +0100

    Implement CircularListTest and related class.

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,68 @@
+package lab01.tdd;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+
+    private final List<Integer> list;
+    private int currentIdx;
+
+    public CircularListImpl() {
+        list = new LinkedList<>();
+        currentIdx = 0;
+    }
+
+    @Override
+    public void add(int element) {
+        list.add(element);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        if(list.isEmpty())
+            return Optional.empty();
+
+        int result = list.get(currentIdx);
+        currentIdx = nextIndex(currentIdx);
+        return Optional.of(result);
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        if(list.isEmpty())
+            return Optional.empty();
+
+        currentIdx = previousIndex(currentIdx);
+        return Optional.of(list.get(currentIdx));
+    }
+
+    @Override
+    public void reset() {
+        currentIdx = 0;
+    }
+
+    @Override
+    public Optional<Integer> next(SelectStrategy strategy) {
+        return Optional.empty();
+    }
+
+    private int nextIndex(int idx) {
+        return ((idx + 1) % size());
+    }
+
+    private int previousIndex(int idx) {
+        return (idx - 1 + size()) % size();
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,94 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private final static int DUMMY_ELEMENT = 5;
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach(){
+        circularList = new CircularListImpl();
     }
 
+    @Test
+    void testAddElement() {
+        circularList.add(DUMMY_ELEMENT);
+        Optional<Integer> result = circularList.next();
+        if(result.isPresent()) {
+            assertEquals(result.get(), DUMMY_ELEMENT);
+        } else {
+            fail("No element has been added to the list");
+        }
+    }
+
+    @Test
+    void testListSize() {
+        int actualSize = 1;
+        circularList.add(DUMMY_ELEMENT);
+        assertEquals(circularList.size(), actualSize);
+    }
+
+    @Test
+    void testInitialEmptyList() {
+        assertTrue(circularList.isEmpty());
+    }
+
+    @Test
+    void testNextElementMissing() {
+        assertFalse(circularList.next().isPresent());
+    }
+
+    @Test
+    void testPreviousElementRetrieval() {
+        circularList.add(DUMMY_ELEMENT);
+        Optional<Integer> previousElement = circularList.previous();
+        assertTrue(previousElement.isPresent());
+        assertEquals(previousElement.get(), DUMMY_ELEMENT);
+    }
+
+    @Test
+    void testPreviousElementMissing() {
+        assertFalse(circularList.previous().isPresent());
+    }
+
+    @Test
+    void testResetList() {
+        for(int i : List.of(1,2,3)) {
+            circularList.add(i);
+        }
+        Optional<Integer> tmpResult;
+
+        tmpResult = circularList.next();
+        assertTrue(tmpResult.isPresent());
+        assertEquals(tmpResult.get(), 1);
+
+        tmpResult = circularList.next();
+        assertTrue(tmpResult.isPresent());
+        assertEquals(tmpResult.get(), 2);
+
+        circularList.reset();
+
+        tmpResult = circularList.next();
+        assertTrue(tmpResult.isPresent());
+        assertEquals(tmpResult.get(), 1);
+    }
+
+    @Disabled
+    @Test
+    void testNextWithStrategy() {
+
+    }
 }
