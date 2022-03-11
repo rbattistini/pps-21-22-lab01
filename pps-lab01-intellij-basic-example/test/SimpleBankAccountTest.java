f26cbfb74fc032b8865f87f0a6a8cab26fec71aa
Riccardo Battistini <riccardo.battistini@protonmail.com>
2022-03-11 10:00:40 +0100

    Update SimpleBankAccountTest.java

--- pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountTest.java
@@ -12,15 +12,15 @@ class SimpleBankAccountTest extends BasicSimpleBankAccountTest {
     @Override
     @BeforeEach
     public void beforeEach(){
-        int userId = 1;
+        final int userId = 1;
         accountHolder = new AccountHolder("Mario", "Rossi", userId);
         bankAccount = new SimpleBankAccount(accountHolder, EMPTY_BALANCE);
     }
 
     @Test
     void testWithdraw() {
-        double depositedAmount = 100.0;
-        double withdrawnAmount = 80.0;
+        final int depositedAmount = 100;
+        final int withdrawnAmount = 80;
         bankAccount.deposit(accountHolder.getId(), depositedAmount);
         bankAccount.withdraw(accountHolder.getId(), withdrawnAmount);
         assertEquals(depositedAmount - withdrawnAmount, bankAccount.getBalance());
@@ -28,8 +28,8 @@ class SimpleBankAccountTest extends BasicSimpleBankAccountTest {
 
     @Test
     void testDisallowedWithdraw() {
-        final double depositedAmount = 100.0;
-        final double withdrawnAmount = 120.0;
+        final int depositedAmount = 100;
+        final int withdrawnAmount = 120;
         bankAccount.deposit(accountHolder.getId(), depositedAmount);
         bankAccount.withdraw(INVALID_USER_ID, withdrawnAmount);
         Assertions.assertEquals(depositedAmount, bankAccount.getBalance());
